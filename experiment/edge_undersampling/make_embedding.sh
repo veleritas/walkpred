8565da0e58f50a53c6f4f005314c2fe9b41676a6
Tong Shu Li <tongli@scripps.edu>
2018-04-05 16:53:25 -0700

    Add embedding caller
    Bash script to call Deepwalk to generate embeddings. Parameters
    are slightly modified to reduce the total number of walks per node
    to decrease runtime.

---
+++ experiment/edge_undersampling/make_embedding.sh
@@ -0,0 +1,14 @@
+fbase=$1
+idx=$2
+
+echo "Running deepwalk for $fbase fold $idx"
+
+pathb="/home/tongli/walkpred/experiment/edge_undersampling/tmp/data"
+
+cd ~/deepwalk
+
+source venv/bin/activate
+
+deepwalk --input "${pathb}/adjlist/${fbase}_adj_${idx}.txt" --output "${pathb}/embeddings/${fbase}_embedding_${idx}.txt" --representation-size 128 --number-walks 40 --window-size 10 --workers 4
+
+deactivate
